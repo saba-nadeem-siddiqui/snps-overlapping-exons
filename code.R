f505e004e533360a49b2943461bde10629ded7a7
Saba Nadeem <saba21.sns@gmail.com>
2026-09-13 14:59:07 +0530

    Calculate genomic ranges

---
+++ code.R
@@ -0,0 +1,41 @@
+
+library(BiocManager)
+library(GenomicRanges)
+library(readxl)
+
+# Read the files
+exons <- read.csv("C:/Users/saba1/Documents/PROJECTS/Learning/snps-on-exon/Exons.csv")
+snps <- read.csv("C:/Users/saba1/Documents/PROJECTS/Learning/snps-on-exon/SNP.csv")
+
+
+
+# Store the value of each column in a vector (exons)
+exonChrValue <- exons$Chromosome
+
+exonStartValue <- as.numeric(exons$exonStart)
+
+exonEndValue <- as.numeric(exons$exonEnd)
+
+# Calculate genomic ranges (exons)
+GRexon <- GRanges(seqnames = exonChrValue,
+                   ranges = IRanges(start = exonStartValue,
+                                    end = exonEndValue))
+
+
+
+# Store the value of each column in a vector (snps)
+snpChrValue <- snps$Chromosome
+
+snpStartValue <- as.numeric(snps$snpStart)
+
+snpEndValue <- as.numeric(snps$snpEnd)
+
+# Calculate genomic ranges (snps)
+GRsnp <- GRanges(seqnames = snpChrValue,
+                 ranges = IRanges(start = snpStartValue,
+                                  end = snpEndValue))
+
+
+
+
+
